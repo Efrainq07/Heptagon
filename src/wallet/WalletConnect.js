b37934079dd3ba631270655ea5971e237acdc167
CTIN2019 <ctin2019@MacBook-Air-de-CTIN2019.local>
2022-08-20 11:29:00 -0500

    WalletConnect refinado v2 - sin usar ABI

--- src/wallet/WalletConnect.js
+++ src/wallet/WalletConnect.js
@@ -10,7 +10,10 @@ const providerOptions = {
     walletconnect: {
       package: WalletConnectProvider, // required
       options: {
-        infuraId: "2DblxUByzOnOipHg8YnNNi3Z3lO" // required
+        infuraId: "2DblxUByzOnOipHg8YnNNi3Z3lO", // required
+        rpc: {
+          80001: "https://polygon-mumbai.infura.io/v3/9f59f26af4144269a44b8ce1c426d028"
+          },
       }
     }
   };
@@ -22,16 +25,42 @@ const web3modal = new Web3Modal(
         cacheProvider : true,
         providerOptions
     }
+    
 );
 
+// export default async function connectWallet() {
+//     var provider = await web3modal.connect()
+
+//     await provider.enable();
+//     var web3 = new Web3(provider)
+
+//     await window.ethereum.send('eth_requestAccounts')
+//     var accounts = await web3.eth.getAccounts()
+//     account = accounts[0]
+//     document.getElementById('wallet-address').textContent = account; 
+//     //contract = new web3.eth.Contract(ABI, ADDRESS);
+// };
+
 export default async function connectWallet() {
+    console.log("yes")
     var provider = await web3modal.connect()
 
+    await provider.enable();
     var web3 = new Web3(provider)
 
-    await window.ethereum.send('eth_requestAccounts')
-    var accounts = await web3.eth.getAccounts()
-    account = accounts[0]
-    document.getElementById('wallet-address').textContent = account; 
-    //contract = new web3.eth.Contract(ABI, ADDRESS);
+    // Subscribe to accounts change
+    provider.on("accountsChanged", function (accounts) {
+      console.log(accounts);
+    });
+    // Subscribe to chainId change
+    provider.on("chainChanged", function (chainId) {
+      console.log(chainId);
+    });
+    // Subscribe to session disconnection
+    provider.on("disconnect", function (code, reason) {
+      console.log(code, reason);
+    });
+    const account = web3.eth.accounts.currentProvider.accounts[0] 
+    
+    
 };
