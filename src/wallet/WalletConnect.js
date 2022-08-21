ed0870157dd47632b0453a257d3d2c4e1f6e36c1
Efrainq07 <efrainq07@hotmail.com>
2022-08-21 04:59:37 -0500

    [WalletConnect] Fix and initial integration

--- src/wallet/WalletConnect.js
+++ src/wallet/WalletConnect.js
@@ -1,110 +1,36 @@
-import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import Web3 from "web3"
-import {createClient} from 'urql'
 import { ethers } from "ethers" ;
+import { requestChallengeQuery, authenticateQuery } from '../graphql-api/APIQueries';
 
 
-const API_URL = 'https://api-mumbai.lens.dev';
+async function signText(provider,address,connection,text){
 
-var account = null
-var contract = null
+  let signature = undefined
+  const signer = provider.getSigner()
 
-// export default async function connectWallet() {
-//     var provider = await web3modal.connect()
-
-//     await provider.enable();
-//     var web3 = new Web3(provider)
-
-//     await window.ethereum.send('eth_requestAccounts')
-//     var accounts = await web3.eth.getAccounts()
-//     account = accounts[0]
-//     document.getElementById('wallet-address').textContent = account; 
-//     //contract = new web3.eth.Contract(ABI, ADDRESS);
-// };
-
-// firmar 
-
-
-
-// Retos conchesumare 
-
-
-const client = createClient(
-  {
-    url: API_URL
+  if (connection.wc) {
+    signature = await provider.send(
+        'personal_sign',
+        [ ethers.utils.hexlify(ethers.utils.toUtf8Bytes(text)), address.toLowerCase() ]
+    );
   }
-)
+  else { 
+    signature = await signer.signMessage(text)
+  }
 
-async function generateChallenge(address){
+  return signature
 
-   console.log(address)
-const challengeQuery = `query Challenge {
-  challenge(request: { address: "${address}"  }) {
-  text
-  } 
-  }`
-  const response = await client.query(challengeQuery).toPromise()
-  console.log("response Challenge",response)
-  return response
 }
 
 export async function handleAuth(connection,provider) {
   const address = (await connection.eth.getAccounts())[0]
-  console.log("address-> ",address)
-  let challenge = await generateChallenge(address)
-  // Signature va aqui 
-  const signer = provider.getSigner()
+
   
-  let signature = "undefined"
-  let challengeText = challenge.data.challenge.text
-  if (connection.wc) {
+  let challengeText = await requestChallengeQuery(address)
 
-    signature = await provider.send(
-        'personal_sign',
-        [ ethers.utils.hexlify(ethers.utils.toUtf8Bytes(challengeText)), address.toLowerCase() ]
-    );
-  }
-  else { 
-    signature = await signer.signMessage(challengeText)
-  }
+  let signature = await signText(provider,address,connection,challengeText)  
+  
+  const response = await authenticateQuery(address,signature)
 
-  const authenticationQuery = `
-  mutation Authenticate {
-  authenticate(request: {
-    address: "${address}",
-    signature: "${signature}"
-  }) {
-    accessToken
-    refreshToken
-  }
-}
-`
-const response = await client.mutation(authenticationQuery).toPromise()
-console.log("response Auth",response)
+  console.log({response})
 }
 
-// export function createAuth(address, signature) {
-
-//   const AUTHENTICATION = `
-//   mutation Authenticate {
-//     authenticate(request: {
-//       address: $address ,
-//       signature: $signature
-//     }) {
-//       accessToken
-//       refreshToken
-//     }
-//   }
-// `
-
-// return apolloClient.mutate({
-//   mutation: gql(AUTHENTICATION),
-//   variables: {
-//     request: {
-//       address,
-//       signature,
-//     },
-//   },
-// })
-// }
---
+++ src/wallet/walletUtilities.js
@@ -0,0 +1,44 @@
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3Modal from "web3modal";
+import Web3 from "web3"
+import { ethers } from "ethers" ;
+
+import { handleAuth  } from './WalletConnect';
+
+const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider, 
+      options: {
+        infuraId: "2DblxUByzOnOipHg8YnNNi3Z3lO", 
+        rpc: {
+          80001: "https://polygon-mumbai.infura.io/v3/9f59f26af4144269a44b8ce1c426d028"
+          },
+      }
+    }
+  };
+
+const web3modal = new Web3Modal(
+    {
+        network : "mumbai",
+        theme : "dark",
+        cacheProvider : true,
+        providerOptions
+    } 
+);
+
+export const handleConnectWallet = async function connectWallet({setAccount,setWeb3modal,setProvider}) {
+    var provider = await web3modal.connect()
+
+    await provider.enable();
+    var web3 = new Web3(provider)
+    const acc = await web3.eth.getAccounts(); 
+
+    var provider2 =  new ethers.providers.Web3Provider(provider);
+
+    
+    handleAuth(web3,provider2);
+
+    setAccount(acc[0]);
+    setWeb3modal(web3modal); 
+    setProvider(provider);
+};
